a6ad2de850eafced9fa799981c7f6d4e0f7db4ec
keshav119 <channakeshav99.ck@gmail.com>
2024-02-16 15:58:11 +0530

    added user admin crud routes

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ mongoose.connect(process.env.MONGO_URI)
   app.use(express.json());
 
   app.use("/api/auth", authRoute);
+  app.use("/api/users", userRoute);
 
 app.listen(3000, () => {
     console.log("Backend is running");
