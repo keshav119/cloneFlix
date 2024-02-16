fd380e1b8a5f8badfd19723b588291e0293db048
keshav119 <channakeshav99.ck@gmail.com>
2024-02-16 13:56:39 +0530

    finished the user Authentication login and register process

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/user");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 //Register
 router.post("/register", async (req,res) =>{
@@ -30,7 +31,15 @@ router.post("/login", async (req,res)=>{
         originalPassword !== req.body.password &&
           res.status(401).json("Wrong password or email");
 
-        res.status(200).json(user);
+          const accessToken = jwt.sign(
+            { id: user._id, isAdmin: user.isAdmin },
+            process.env.SECRET_KEY,
+            { expiresIn: "5d" }
+            );
+
+        const { password, ...info } = user._doc;
+
+        res.status(200).json({ ...info, accessToken });
     }catch(err){
         res.status(500).json(err);
     }
