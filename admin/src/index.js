b89ea5288d2a3b0d4092507e1f1c685ea405ec7e
keshav119 <channakeshav99.ck@gmail.com>
2024-02-21 20:07:01 +0530

    setup admin ui

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { AuthContextProvider } from "./context/authContext/AuthContext";
+import { MovieContextProvider } from "./context/movieContext/MovieContext";
+import { ListContextProvider } from "./context/listContext/ListContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <AuthContextProvider>
+      <MovieContextProvider>
+        <ListContextProvider>
+          <App />
+        </ListContextProvider>
+      </MovieContextProvider>
+    </AuthContextProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
